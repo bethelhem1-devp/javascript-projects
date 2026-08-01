2ba15630e5cb62b6ed2fbbc87aa8f8e9e17d149e
bethelhem1-devp <behttytamrat@gmail.com>
2026-08-01 22:28:28 +0300

    Add files via upload

---
+++ for_of.js
@@ -0,0 +1,27 @@
+//how for..of works
+function printCharacters(str) {
+  for (const char of str) {
+    console.log(char);
+  }
+}
+printCharacters("hello");
+
+function getMatchedWordCount(sentence, match) {
+  let count = 0;
+
+  for (const word of sentence) {
+    console.log(`Checking ${word} against ${match} | Running count: ${count}`);
+    if (word === match) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(
+  getMatchedWordCount(
+    ["I", "really", "really", "really", "like", "to", "code"],
+    "really"
+  )
+);
